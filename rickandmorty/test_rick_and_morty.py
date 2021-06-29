3c8c89b9a96a238b171b52779fd9e16545889175
Brittany Bennett <thebrittanybennett@gmail.com>
2021-06-29 13:42:37 -0400

    Added location and episode cleaning functions

--- rickandmorty/test_rick_and_morty.py
+++ rickandmorty/test_rick_and_morty.py
@@ -34,10 +34,7 @@ def test_character_cleanup():
         "image": "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
         "location_id": 20,
         "name": "Rick Sanchez",
-        "origin": {
-            "name": "Earth (C-137)",
-            "url": "https://rickandmortyapi.com/api/location/1",
-        },
+        "origin_id": 1,
         "species": "Human",
         "status": "Alive",
         "type": "",
@@ -45,6 +42,50 @@ def test_character_cleanup():
     }
 
     assert rick_and_morty.character_cleanup(input) == expected_output
+
+def test_episode_cleanup():
+    input = {'id': 1,
+        'name': 'Pilot',
+        'air_date': 'December 2, 2013',
+        'episode': 'S01E01',
+        'characters': ['https://rickandmortyapi.com/api/character/1',
+            'https://rickandmortyapi.com/api/character/2',
+            'https://rickandmortyapi.com/api/character/35'],
+        'created': '2017-11-10T12:56:33.798Z'
+            }
+
+    expected_output = {'id': 1,
+        'name': 'Pilot',
+        'air_date': 'December 2, 2013',
+        'episode': 'S01E01',
+        'character_ids': [1, 2, 35],
+        'created': '2017-11-10T12:56:33.798Z'
+        }
+
+    assert rick_and_morty.episode_cleanup(input) == expected_output
+
+def test_location_cleanup():
+
+    input = {'id': 1,
+        'name': 'Earth (C-137)',
+        'type': 'Planet',
+        'dimension': 'Dimension C-137',
+        'residents': ['https://rickandmortyapi.com/api/character/38', 'https://rickandmortyapi.com/api/character/45', 'https://rickandmortyapi.com/api/character/71'],
+        'url': 'https://rickandmortyapi.com/api/location/1',
+        'created': '2017-11-10T12:42:04.162Z'
+        }
+
+    expected_output = {'id': 1,
+        'name': 'Earth (C-137)',
+        'type': 'Planet',
+        'dimension': 'Dimension C-137',
+        'resident_ids': [38, 45, 71],
+        'url': 'https://rickandmortyapi.com/api/location/1',
+        'created': '2017-11-10T12:42:04.162Z'
+        }
+
+    assert rick_and_morty.location_cleanup(input) == expected_output
+
 
 
 def test_extract_id_from_url():
